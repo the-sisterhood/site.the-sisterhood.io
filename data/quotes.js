713f9bf9fe738b1cd7da8bb1c3a62237c1722a5c
Charlie Kobayashi <sparkychildcharlie@gmail.com>
2017-06-03 09:32:24 -0400

    Quotes: add new quotes

--- data/quotes.js
+++ data/quotes.js
@@ -8,5 +8,25 @@ var Quotes = window.Quotes = [
         quote: "I hate every single witch doctor alt and in complete honesty wish I could ban every single one of you",
         by: "TheWhoDoctor",
         date: "May 30, 2017, 20:18:42",
-    }
+    },
+    {
+        quote: "I'm not hosting an official with spammed witchdoctor alts b/c i'll host error",
+        by: "TheWhoDoctor",
+        date: "June 2, 2017, 21:00:49",
+    },
+    {
+        quote: "I wish they did it agian / because i can gameban them for longer",
+        by: "TheWhoDoctor",
+        date: "June 2, 2017, 21:04:42",
+    },
+    {
+        quote: ".l WitchDoctorFoxie / don't wanna NL / and I don't wanna have three WitchDoctors entering the next few days",
+        by: "shubaka17",
+        date: "June 2, 2017, 22:12:03",
+    },
+    {
+        quote: " I'm still on foxie / for being a witch doctor",
+        by: "shubaka17",
+        date: "June 2, 2017, 22:14:56",
+    },
 ];
